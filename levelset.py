2d3419c5e7e3d08b61cd3973c8e78f8a26246b48
Agustinus Kristiadi <kristiadi7@gmail.com>
2016-09-28 17:09:24 +0700

    Cleanup

--- levelset.py
+++ levelset.py
@@ -2,6 +2,7 @@ import numpy as np
 import scipy.ndimage
 import scipy.signal
 import matplotlib.pyplot as plt
+from skimage import io, color
 
 
 def imshow(img):
@@ -9,20 +10,38 @@ def imshow(img):
     plt.show()
 
 
+def grad_norm(grad):
+    """ Calculate gradient magnitude (2-norm) """
+    return np.sqrt(np.sum(np.square(grad), axis=0))
+
+
 dt = 1.
 alpha = 1.
 
 
 if __name__ == '__main__':
-    img = plt.imread('twoObj.bmp')
-    img = img - np.mean(img)
-    img_smooth = scipy.ndimage.filters.median_filter(img, 5)
+    # Load image and convert to grayscale
+    img = io.imread('data/img/twoObj.bmp')
+    img = color.rgb2gray(img)
 
-    dimg_y, dimg_x = np.gradient(img_smooth)
-    dimg_norm = np.sqrt(dimg_x**2 + dimg_y**2)
+    assert len(img.shape) == 2
+
+    # Preprocessing: mean substraction and denoising (by applying Gaussian blur)
+    img = img - np.mean(img)
+    img_smooth = scipy.ndimage.filters.gaussian_filter(img, 0)
+
+    # Derive image edge features f from its gradient
+    dimg = np.gradient(img_smooth)
+    dimg_norm = grad_norm(dimg)
 
     f = np.exp(-alpha * dimg_norm)
+    # f = 1. / (1. + dimg_norm**2)
 
+    assert img.shape == f.shape
+
+    # Initialize surface function phi
+    # 5px from image border with value 5 and -5 elsewhere
+    # The 0 level curve would be a square curve
     phi = 5 * np.ones_like(img)
     phi[5:-5, 5:-5] = -5.
 
@@ -30,9 +49,11 @@ if __name__ == '__main__':
     imshow(f)
 
     for i in range(100):
-        dphi_y, dphi_x = np.gradient(phi)
-        dphi_norm = np.sqrt(dphi_x**2 + dphi_y**2)
+        # Calculate grad phi's norm
+        dphi = np.gradient(phi)
+        dphi_norm = grad_norm(dphi)
 
+        # Solve level set equation's PDE
         phi = phi + dt * (f * dphi_norm)
 
         if i % 10 == 0:
