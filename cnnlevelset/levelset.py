9a4e796edd1a7a4e401b556ea8dbe901d4961b00
Agustinus Kristiadi <kristiadi7@gmail.com>
2016-10-31 11:59:05 +0700

    Use geodesic levelset

--- cnnlevelset/levelset.py
+++ cnnlevelset/levelset.py
@@ -10,9 +10,23 @@ def imshow(img):
     plt.show()
 
 
-def grad_norm(grad):
-    """ Calculate gradient magnitude (2-norm) """
-    return np.sqrt(np.sum(np.square(grad), axis=0))
+def norm(x):
+    return np.sqrt(np.sum(np.square(x), axis=0))
+
+
+def curvature(phi):
+    dphi = np.gradient(phi)
+    dphiy, dphix = dphi
+    dphiyy, dphiyx = np.gradient(dphiy)
+    dphixy, dphixx = np.gradient(dphix)
+
+    kappa = (dphixx*dphiy**2 - 2*dphix*dphiy*dphixy + dphiyy*dphix**2) / (1. + norm(dphi)**3)
+
+    return kappa
+
+
+def stopping_fun(img):
+    return 1. / (1. + norm(np.gradient(img))**2)
 
 
 dt = 1.
@@ -30,14 +44,8 @@ if __name__ == '__main__':
     img = img - np.mean(img)
     img_smooth = scipy.ndimage.filters.gaussian_filter(img, 2)
 
-    # Derive image edge features f from its gradient
-    dimg = np.gradient(img_smooth)
-    dimg_norm = grad_norm(dimg)
-
-    f = np.exp(-alpha * dimg_norm)
-    # f = 1. / (1. + dimg_norm**2)
-
-    assert img.shape == f.shape
+    g = stopping_fun(img_smooth)
+    dg = np.gradient(g)
 
     # Initialize surface function phi
     # 5px from image border with value 5 and -5 elsewhere
@@ -45,21 +53,29 @@ if __name__ == '__main__':
     phi = 5 * np.ones_like(img)
     phi[5:-5, 5:-5] = -5.
 
-    imshow(img_smooth)
-    imshow(f)
-
     for i in range(100):
         # Calculate grad phi's norm
         dphi = np.gradient(phi)
-        dphi_norm = grad_norm(dphi)
+
+        dphiy, dphix = dphi
+        dphiyy, dphiyx = np.gradient(dphiy)
+        dphixy, dphixx = np.gradient(dphix)
+        kappa = curvature(phi)
+
+        dphi_norm = norm(dphi)
+        kappa = curvature(phi)
+        dot = dg[0]*dphi[0] + dg[1]*dphi[1]
 
         # Solve level set equation's PDE
-        phi = phi + dt * (f * dphi_norm)
+        phi = phi + dt * (g*dphi_norm + g*kappa*dphi_norm + dot)
 
         if i % 10 == 0:
             plt.imshow(img, cmap='Greys_r')
             plt.hold(True)
-            plt.contour(phi, 0, colors='r', linewidths=[5])
+            plt.contour(phi, 0, colors='r', linewidths=[3])
             plt.draw()
             plt.hold(False)
             plt.show()
+
+            input()
+
